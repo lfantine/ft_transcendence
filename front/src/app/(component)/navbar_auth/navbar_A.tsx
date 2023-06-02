b6004aeeaff42d38ad052ce25c37c7a19851f84a
Nartyy <129529306+lfantine@users.noreply.github.com>
2023-06-03 01:59:48 +0200

    new version

--- front/src/app/(component)/navbar_auth/navbar_A.tsx
+++ front/src/app/(component)/navbar_auth/navbar_A.tsx
@@ -1,18 +1,70 @@
 import Link from 'next/link';
-import React from 'react'
+import React, { useState } from 'react';
+import styles from './navbar_A.module.css';
 
 export interface INavbar {
 	navActive: boolean;
 	testing: boolean;
 }
 
+export const sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
+
 const NavbarA: React.FC<INavbar> = (Content) => {
+
+const [activeBand, setActiveBand] = useState(true);
+const [activeMusic, setActiveMusic] = useState(false);
+// const [music, setmusic] = useState(new Audio('vinil_music.mp3'));
+
+const handleNav = async (e: any) => {
+	e.preventDefault();
+	if (activeBand === false)
+	{
+		setActiveBand(!activeBand);
+		await sleep(10);
+		const nav = document.getElementById('nav');
+		if (nav === null)
+			console.log('null');
+		nav?.classList.remove(styles['close']);
+		nav?.classList.add(styles['open']);
+	}
+	else
+	{
+		const nav = document.getElementById('nav');
+		nav?.classList.remove(styles['open']);
+		nav?.classList.add(styles['close']);
+		setActiveBand(!activeBand);
+	}
+}
+
+const handleMusic = async (e: any) => {
+	e.preventDefault();
+	setActiveMusic(!activeMusic);
+	await sleep(10);
+	const el = document.getElementById('music');
+	const o = document.getElementById('odio');
+	if (!activeMusic)
+	{
+		el?.classList.add(styles['tourne']);
+		o.play();
+	}
+	else
+	{
+		el?.classList.remove(styles['tourne']);
+		o.pause();
+	}
+}
+
 	return (
-		<div className='navbar'>
-			<div className='nav_C one'><Link className='link' href="" rel='acceuil'>ACCUEIL</Link></div>
-			<div className='nav_C'><Link className='link' href="/login" rel='login'>CONNECT</Link></div>
-			<div className='nav_C logo'>😁</div>
-		</div>
+		<main>
+			<div className={styles.bar}>
+				<img src='lanterne.png' className={styles.lant} onClick={handleNav}></img>
+				<div className={styles.band} id='nav'>
+					<div className={styles.link}><Link href="" className={styles.L}>HOME</Link></div>
+					<div className={styles.link}><Link href="/login" className={styles.L}>CONNECTION</Link></div>
+				</div>
+				<div className={styles.music} onClick={handleMusic}><img src='vinil.png' className={styles.vinil} id='music'></img><audio src='vinil_music.mp3' id='odio'></audio></div>
+			</div>
+		</main>
 	);
 }
   
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import Image from 'next/image'
-import * as styles from './page.module.css'
+import Image from 'next/image';
 import NavbarA from './(component)/navbar_auth/navbar_A';
 import { resolve } from 'path';
 import { useQuery } from '@tanstack/react-query';
@@ -12,6 +11,7 @@ import Api from './api/api';
 import { useRouter } from 'next/navigation';
 import { checkLogin } from './(utils)/isLogin';
 import { AuthResponse } from './(auth)/auth.api';
+import styles from './page.module.css';
 
 interface Data {
   userId: number
@@ -21,36 +21,28 @@ export default function Home() {
 
   const { push } = useRouter();
 
-  useEffect(() => {
-		const localData = localStorage.getItem('log');
-		if (localData === 'yes'){
-			push('/dashboard');
-			console.log('is already logged');
-			return ;
-		}
-		const userData = checkLogin();
-		userData.then(function(data: AuthResponse | undefined) {
-			if (data !== undefined){
-				localStorage.setItem('log', 'yes');
-				push('/dashboard');
-				console.log('is already logged');
-			}
-			else
-				localStorage.setItem('log', 'no');
-		})
-	}, [])
+//   useEffect(() => {
+// 		const localData = localStorage.getItem('log');
+// 		if (localData === 'yes'){
+// 			push('/dashboard');
+// 			console.log('is already logged');
+// 			return ;
+// 		}
+// 		const userData = checkLogin();
+// 		userData.then(function(data: AuthResponse | undefined) {
+// 			if (data !== undefined){
+// 				localStorage.setItem('log', 'yes');
+// 				push('/dashboard');
+// 				console.log('is already logged');
+// 			}
+// 			else
+// 				localStorage.setItem('log', 'no');
+// 		})
+// 	}, [])
 
   return (
     <main>
       <NavbarA navActive={true} testing={true}/>
-      <h1 className='title'>Accueil du Site de ft_transcendance :3</h1>
-      <section className='site_desc'>
-        <h2>ft_transcendance be like :</h2>
-        <div className='photo'>
-          <img className='game_img' src='/img_acceuil_game.jpg' alt='game_img'/>
-        </div>
-        <h2>NONE</h2>
-      </section>
     </main>
   );
 }
